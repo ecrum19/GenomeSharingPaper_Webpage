3e09ae3c96da7e73819083aa8c4c297a58b2b790
ecrum19 <elicrum88@gmail.com>
2025-09-29 09:55:31 +0200

    updated initiative data

--- fix_csv.py
+++ fix_csv.py
@@ -2,8 +2,8 @@ import csv
 import os
 
 # Define input and output file paths
-input_file_path = 'GenomeInitiativeCountries.csv'
-output_file_path = 'J_GenomeInitiativeCountries.csv'
+input_file_path = 'SupplementalFile4.csv'
+output_file_path = 'GenomeInitiativeCountries.csv'
 
 # Open the input CSV file for reading
 with open(input_file_path, mode='r') as input_file:
@@ -13,6 +13,6 @@ output_file = open(output_file_path, mode='w')
 
 for row in the_file[:-1]:
     line = row.split(',')
-    print(line)
-    modified_row = "['{}',{}],".format(line[0], line[1])
+    # print(line)
+    modified_row = "{},{},{},{},".format(line[0].strip(), line[1].strip(), line[4].strip(), line[5].strip())
     output_file.write(modified_row + '\n')
